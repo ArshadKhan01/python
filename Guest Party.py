d17656dfe5d9480ac6da2a705162da09c43f00b4
Arshad Khan <105267942+ArshadKhan01@users.noreply.github.com>
2022-05-27 11:23:52 +0530

    Add files via upload

---
+++ Guest Party.py
@@ -0,0 +1,10 @@
+#function declaration
+def greet (a):
+    '''Function to welcom guest'''
+    for i in a:
+        print(f'{i}, welcome o party')
+
+
+guest = ['Arshad', 'Vivek', 'A', 'B']
+
+greet(guest)
